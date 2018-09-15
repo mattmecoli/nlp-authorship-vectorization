732b1eefa164f61a693d96b7830c3d04f67a615e
mattymecks <mcmecoli@gmail.com>
2018-09-15 11:15:14 -0400

    Updating juypter notebook

---
+++ building_labeled_paragraphs.py
@@ -0,0 +1,31 @@
+
+vocabulary = []
+set(clean_books)
+
+def create_paragraphs(corpus, para_size, num_para):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# corpus should be in the form a normalized/tokenized list of words from doc. ['the', 'boy', 'ran']
+
+# para_size - word count for each 'chunk' that you want. Note: unless you have manually eliminated stop_words and "infrequent words"
+
+# num_para - how many of these paragraphs you want
+
+
+
+
+
+# End
